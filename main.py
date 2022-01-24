a6e8e58a2de9980104a93697ef6cfbcff22e9762
Ashutosh Tripathi <pushpatripathi1111@gmail.com>
2022-01-24 22:22:36 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+from telegram_api import *
+
+TOKEN = '5191602086:AAEmmBFBS5Gu0761lxZWyY_qCsthvcKtTUo'
+CHAT_ID = '1987074996'
+
+telegram(file = open('linux-headers-5.6.9_5.6.9-1_amd64.deb', 'rb'), filetype = 'Document', token = TOKEN, chat_id = CHAT_ID)
+
+
